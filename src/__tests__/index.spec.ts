321cb8f882937e1a3e7ec8ae519fa20c5abbd409
Josh Welham <josh@aluminati.net>
2018-09-30 17:40:41 +0100

    enabled jest import and typescript support

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,5 +1,15 @@
-import * as client from '@/index';
-
-test('adds 1 + 2 to equal 3', () => {
-    expect(client.moduleName).toBe('core');
-});
+// import * as client from '@/index';
+//
+// test('adds 1 + 2 to equal 3', () =>
+// {
+// 	expect(client.moduleName).toBe('core');
+// 	console.log(client.moduleName);
+// 	// asdf
+// });
+//
+// test('adds 1 + 2 toa equal 3', () =>
+// {
+// 	expect(client.moduleName).toBe('core');
+// 	console.log(client.moduleName);
+// 	// asdf
+// });

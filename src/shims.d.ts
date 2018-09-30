cf92950a4b973e7fb9790b56ff9eccc6d25ccfa6
Josh Welham <josh@aluminati.net>
2018-09-30 17:43:18 +0100

    linting

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,2 +1,2 @@
-declare module "config" {
+declare module 'config' {
 }
--- testConfigs/jest-setup.ts
+++ testConfigs/jest-setup.ts
@@ -1,11 +1,11 @@
-import 'jest';
+// import 'jest';
 
 (process as any).browser = false;
 process.env.NODE_ENV = 'test';
 
 global.console = {
-  debug: jest.fn(),
-  error: jest.fn(),
-  info: jest.fn(),
-  log: jest.fn(),
+	debug: jest.fn(),
+	error: jest.fn(),
+	info: jest.fn(),
+	log: jest.fn()
 } as any;

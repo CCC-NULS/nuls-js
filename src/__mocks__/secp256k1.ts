ee425baf8ff443aa264d2f1073a40560b5bc1af6
Josh Welham <josh@aluminati.net>
2018-10-11 00:16:55 +0100

    linting rules

--- src/__mocks__/secp256k1.ts
+++ src/__mocks__/secp256k1.ts
@@ -1,6 +1,6 @@
 module.exports = {
 	privateKeyVerify: jest.fn().mockReturnValue(true),
 	publicKeyCreate: jest.fn().mockReturnValue(Buffer.from('02d772f1649c142494483e358b915573f5ba1573c71951117fc9a7db804fc3e64b', 'hex')),
-	sign: jest.fn().mockReturnValue('foo'),
+	sign: jest.fn().mockReturnValue({ signature: 'foo' }),
 	verify: jest.fn().mockReturnValue('bar')
 };

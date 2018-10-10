0b036d42f7dbacb460e28237505be5515bbc28a9
Josh Welham <josh@aluminati.net>
2018-10-11 00:17:09 +0100

    added comments and documentation

--- src/packages/core/newAccount.ts
+++ src/packages/core/newAccount.ts
@@ -1,8 +1,15 @@
-const RIPEMD160 = require('ripemd160');
-const secp256k1 = require('secp256k1');
-const shajs = require('sha.js');
-const bs58 = require('bs58');
-const { randomBytes } = require('crypto');
+import * as bs58 from 'bs58';
+import { randomBytes } from 'crypto';
+import * as RIPEMD160 from 'ripemd160';
+import * as secp256k1 from 'secp256k1';
+import * as shajs from 'sha.js';
+
+interface IGetAccount
+{
+	address: string;
+	privateKey: string;
+	publicKey: string;
+}
 
 class NewAccount
 {
@@ -35,8 +42,13 @@ class NewAccount
 	// 	return Date.now() - this.startTime;
 	// }
 
-	getAccount()
+	/**
+	 * Get the account generated
+	 */
+	public getAccount(): IGetAccount | null
 	{
+		if(!this.address || !this.privateKey || !this.publicKey) return null;
+
 		return {
 			address: this.address,
 			privateKey: this.privateKey,
@@ -44,7 +56,10 @@ class NewAccount
 		};
 	}
 
-	createAccount()
+	/**
+	 * Initiates creating an account
+	 */
+	public createAccount(): void
 	{
 		if(this.privateKey) // If a private key already exists we use it
 		{
@@ -83,32 +98,38 @@ class NewAccount
 	}
 
 	/**
-	 * Validate the private keys
+	 * Validate the private key
 	 */
-	validatePrivateKey()
+	private validatePrivateKey(): void
 	{
-		const msg = randomBytes(32);
-		const sigObj = secp256k1.sign(msg, this.privateKeyBuffer);
+		const msg: Buffer = randomBytes(32);
+		const { signature }: { signature: Buffer } = secp256k1.sign(msg, this.privateKeyBuffer);
 
-		if(!secp256k1.verify(msg, sigObj.signature, this.publicKeyBuffer))
+		if(!secp256k1.verify(msg, signature, this.publicKeyBuffer))
 		{
 			throw new Error('Something went wrong when validating the signature.');
 		}
 	}
 
-	privateKeyHash()
+	/**
+	 * Encrypts the private key buffer using sha256 and hash160
+	 */
+	private privateKeyHash(): Buffer
 	{
 		// sha256hash160
 		// https://github.com/nuls-io/nuls/blob/274204b748ed72fdac150637ee758037d64c7ce5/core-module/kernel/src/main/java/io/nuls/kernel/utils/AddressTool.java#L61
-		const sha = new shajs.sha256().update(this.publicKeyBuffer).digest();
-		const hash = new RIPEMD160().update(sha).digest();
+		const sha: Buffer = new shajs.sha256().update(this.publicKeyBuffer).digest();
+		const hash: Buffer = new RIPEMD160().update(sha).digest();
 
 		return hash;
 	}
 
-	getPublicKeyHash()
+	/**
+	 * Creates the public key hash using encryption, chainId, and addressId
+	 */
+	private getPublicKeyHash(): Buffer
 	{
-		const output = Buffer.allocUnsafe(3);
+		const output: Buffer = Buffer.allocUnsafe(3);
 
 		output.writeInt16LE(this.chainId, 0);
 		output.writeInt8(this.addressType, 2);
@@ -116,16 +137,19 @@ class NewAccount
 		return Buffer.concat([output, this.privateKeyHash()]);
 	}
 
-	createAddress()
+	/**
+	 * Using the public key create the public address
+	 */
+	private createAddress(): string
 	{
-		const publicKeyHash = this.getPublicKeyHash();
+		const publicKeyHash: Buffer = this.getPublicKeyHash();
 
 		// https://github.com/nuls-io/nuls/blob/274204b748ed72fdac150637ee758037d64c7ce5/core-module/kernel/src/main/java/io/nuls/kernel/utils/AddressTool.java#L182
-		const address = bs58.encode(
+		const address: string = bs58.encode(
 			Buffer.concat([
 				publicKeyHash,
 				Buffer.from([
-					publicKeyHash.reduce((xor, value) => xor ^ value)
+					publicKeyHash.reduce((xor: number, value: number) => xor ^ value)
 				])
 			])
 		);
@@ -133,14 +157,20 @@ class NewAccount
 		return address;
 	}
 
-	stringToHex(string?: string)
+	/**
+	 * String to buffer
+	 */
+	private stringToHex(str?: string): Buffer | undefined
 	{
-		if(!string) return undefined;
+		if(!str) return undefined;
 
-		return Buffer.from(string, 'hex');
+		return Buffer.from(str, 'hex');
 	}
 
-	hexToString(hex?: Buffer)
+	/**
+	 * Buffer to string
+	 */
+	private hexToString(hex?: Buffer): string | undefined
 	{
 		if(!hex) return undefined;
 
@@ -160,3 +190,15 @@ class NewAccount
 }
 
 module.exports = NewAccount;
+
+// Running the API
+// https://github.com/nuls-io/API-server
+// cd target
+// java -jar API-Server-1.0-SNAPSHOT.jar
+// http://172.30.20.78:8765/nuls
+// There is an error at -> https://github.com/nuls-io/API-server/blob/eda6320157eb92ca964dff2b2f89c60a9d897bb2/src/main/java/io/nuls/api/server/task/BlockSyncTask.java#L87
+// Database config -> https://github.com/nuls-io/API-server/blob/eda6320157eb92ca964dff2b2f89c60a9d897bb2/src/main/resources/jdbc.properties
+// ^^ When you edit the config don't forget to rebuild the `target` folder by running `mvn clean -DskipTests=true package` at root project level
+// Extracting command line commands
+// https://github.com/nuls-io/nuls/blob/4436795eabe864437de013b83aee0dca0d5400bf/account-module/base/account-rpc/src/main/java/io/nuls/account/rpc/cmd/CreateProcessor.java
+// Database http://172.30.20.78/phpmyadmin/

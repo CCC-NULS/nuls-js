10f76542051cfaae4a97d5537c79d1bdccc88087
Josh Welham <josh@aluminati.net>
2018-10-13 17:07:49 +0100

    super not needed

--- src/packages/core/Account.ts
+++ src/packages/core/Account.ts
@@ -2,8 +2,4 @@ import { NewAccount } from '@/packages/core/account/NewAccount';
 
 export class Account extends NewAccount
 {
-	constructor(privateKey?: string, addressType: number = 1, chainId: number = 8964)
-	{
-		super(privateKey, addressType, chainId);
-	}
 }

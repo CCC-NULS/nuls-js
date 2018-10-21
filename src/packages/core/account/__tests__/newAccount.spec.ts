51c73b1b7494d15c0434c30696d1f1082c0e9b47
Josh Welham <josh@aluminati.net>
2018-10-21 14:35:08 +0100

    test get account function

--- src/packages/core/account/__tests__/newAccount.spec.ts
+++ src/packages/core/account/__tests__/newAccount.spec.ts
@@ -136,9 +136,10 @@ describe('create new accounts', () =>
 			publicKeyCreate.mockReturnValue(Buffer.from('033f4031d22289befe017472bb954b59d9ba043ce67fbc60c50ee3a48c56b89b1f', 'hex'));
 
 			const account = new NewAccount();
-			const foo = account.import('Password1!', '2d5ed8706749f6d7c096772a075c027f56fae4148bacbf6c78b59df09f84b07b');
 
-			expect(foo).toEqual({
+			account.import('Password1!', '2d5ed8706749f6d7c096772a075c027f56fae4148bacbf6c78b59df09f84b07b');
+
+			expect(account.getAccount()).toEqual({
 				address: 'Nse8Ar5XvuPdDCYcTnkK4LDwDNZqTqYx',
 				privateKey: '2d5ed8706749f6d7c096772a075c027f56fae4148bacbf6c78b59df09f84b07b',
 				privateKeyEncrypted: '6f1a067690b4481de3743de3f015da5f172d939b5b1b4842c16977278a9c1fb914adc6079df87c70ab6cef422d6add01',
@@ -159,9 +160,10 @@ describe('create new accounts', () =>
 		{
 			const { NewAccount } = require('@/packages/core/account/NewAccount');
 			const account = new NewAccount();
-			const foo = account.create();
 
-			expect(foo).toEqual({
+			account.create();
+
+			expect(account.getAccount()).toEqual({
 				address: 'Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6',
 				privateKey: '889bba933b77da1360e1fc6314552f3d777a099cca82dcf594c6f3e3287b3c97',
 				publicKey: '02d772f1649c142494483e358b915573f5ba1573c71951117fc9a7db804fc3e64b'

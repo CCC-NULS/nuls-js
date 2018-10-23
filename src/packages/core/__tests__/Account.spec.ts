60eb5af9cd66903fababae36d17c03d1beb82115
Josh Welham <josh@aluminati.net>
2018-10-23 23:25:38 +0100

    tests updated

--- src/packages/core/__tests__/Account.spec.ts
+++ src/packages/core/__tests__/Account.spec.ts
@@ -18,8 +18,8 @@ describe('create new accounts', () =>
 
 				expect(Account.create()).toEqual({
 					address: 'Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6',
-					privateKey: '889bba933b77da1360e1fc6314552f3d777a099cca82dcf594c6f3e3287b3c97',
-					publicKey: '02d772f1649c142494483e358b915573f5ba1573c71951117fc9a7db804fc3e64b'
+					prikey: '889bba933b77da1360e1fc6314552f3d777a099cca82dcf594c6f3e3287b3c97',
+					pubKey: '02d772f1649c142494483e358b915573f5ba1573c71951117fc9a7db804fc3e64b'
 				});
 			});
 
@@ -34,8 +34,8 @@ describe('create new accounts', () =>
 
 				expect(Account.create(undefined, 2, 9000)).toEqual({
 					address: '4fDyBjzqYi5bfqMDT9ggC8SdLtvfw88Yu',
-					privateKey: '2e4fa652034f495089daebb9a390600cb144eafe3fe34fad0750ee9674dfc402',
-					publicKey: '02d772f1649c142494483e358b915573f5ba1573c71951117fc9a7db804fc3e64b'
+					prikey: '2e4fa652034f495089daebb9a390600cb144eafe3fe34fad0750ee9674dfc402',
+					pubKey: '02d772f1649c142494483e358b915573f5ba1573c71951117fc9a7db804fc3e64b'
 				});
 			});
 
@@ -45,9 +45,9 @@ describe('create new accounts', () =>
 
 				expect(Account.create('Password1!')).toEqual({
 					address: 'Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6',
-					privateKey: '889bba933b77da1360e1fc6314552f3d777a099cca82dcf594c6f3e3287b3c97',
-					privateKeyEncrypted: 'b39747ac8e9aa114f6a173d29f57d70082de584704b399b6de0a51804f45f9b24eca1a53ed8b64e9c73b8297b8cc3faf',
-					publicKey: '02d772f1649c142494483e358b915573f5ba1573c71951117fc9a7db804fc3e64b'
+					encryptedPrivateKey: 'b39747ac8e9aa114f6a173d29f57d70082de584704b399b6de0a51804f45f9b24eca1a53ed8b64e9c73b8297b8cc3faf',
+					prikey: '889bba933b77da1360e1fc6314552f3d777a099cca82dcf594c6f3e3287b3c97',
+					pubKey: '02d772f1649c142494483e358b915573f5ba1573c71951117fc9a7db804fc3e64b'
 				});
 			});
 
@@ -57,8 +57,8 @@ describe('create new accounts', () =>
 
 				expect(Account.create()).toEqual({
 					address: 'Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6',
-					privateKey: '889bba933b77da1360e1fc6314552f3d777a099cca82dcf594c6f3e3287b3c97',
-					publicKey: '02d772f1649c142494483e358b915573f5ba1573c71951117fc9a7db804fc3e64b'
+					prikey: '889bba933b77da1360e1fc6314552f3d777a099cca82dcf594c6f3e3287b3c97',
+					pubKey: '02d772f1649c142494483e358b915573f5ba1573c71951117fc9a7db804fc3e64b'
 				});
 
 				const { randomBytes } = require('crypto');
@@ -69,8 +69,8 @@ describe('create new accounts', () =>
 
 				expect(Account.create()).toEqual({
 					address: 'Nse8Ar5XvuPdDCYcTnkK4LDwDNZqTqYx',
-					privateKey: '2d5ed8706749f6d7c096772a075c027f56fae4148bacbf6c78b59df09f84b07b',
-					publicKey: '033f4031d22289befe017472bb954b59d9ba043ce67fbc60c50ee3a48c56b89b1f'
+					prikey: '2d5ed8706749f6d7c096772a075c027f56fae4148bacbf6c78b59df09f84b07b',
+					pubKey: '033f4031d22289befe017472bb954b59d9ba043ce67fbc60c50ee3a48c56b89b1f'
 				});
 			});
 		});
@@ -92,8 +92,8 @@ describe('create new accounts', () =>
 
 				expect(Account.import(null, '2d5ed8706749f6d7c096772a075c027f56fae4148bacbf6c78b59df09f84b07b')).toEqual({
 					address: 'Nse8Ar5XvuPdDCYcTnkK4LDwDNZqTqYx',
-					privateKey: '2d5ed8706749f6d7c096772a075c027f56fae4148bacbf6c78b59df09f84b07b',
-					publicKey: '033f4031d22289befe017472bb954b59d9ba043ce67fbc60c50ee3a48c56b89b1f'
+					prikey: '2d5ed8706749f6d7c096772a075c027f56fae4148bacbf6c78b59df09f84b07b',
+					pubKey: '033f4031d22289befe017472bb954b59d9ba043ce67fbc60c50ee3a48c56b89b1f'
 				});
 			});
 
@@ -106,9 +106,9 @@ describe('create new accounts', () =>
 
 				expect(Account.import('Password1!', '6f1a067690b4481de3743de3f015da5f172d939b5b1b4842c16977278a9c1fb914adc6079df87c70ab6cef422d6add01')).toEqual({
 					address: 'Nse8Ar5XvuPdDCYcTnkK4LDwDNZqTqYx',
-					privateKey: '2d5ed8706749f6d7c096772a075c027f56fae4148bacbf6c78b59df09f84b07b',
-					privateKeyEncrypted: '6f1a067690b4481de3743de3f015da5f172d939b5b1b4842c16977278a9c1fb914adc6079df87c70ab6cef422d6add01',
-					publicKey: '033f4031d22289befe017472bb954b59d9ba043ce67fbc60c50ee3a48c56b89b1f'
+					encryptedPrivateKey: '6f1a067690b4481de3743de3f015da5f172d939b5b1b4842c16977278a9c1fb914adc6079df87c70ab6cef422d6add01',
+					prikey: '2d5ed8706749f6d7c096772a075c027f56fae4148bacbf6c78b59df09f84b07b',
+					pubKey: '033f4031d22289befe017472bb954b59d9ba043ce67fbc60c50ee3a48c56b89b1f'
 				});
 			});
 
@@ -121,9 +121,9 @@ describe('create new accounts', () =>
 
 				expect(Account.import('Password1!', '2d5ed8706749f6d7c096772a075c027f56fae4148bacbf6c78b59df09f84b07b')).toEqual({
 					address: 'Nse8Ar5XvuPdDCYcTnkK4LDwDNZqTqYx',
-					privateKey: '2d5ed8706749f6d7c096772a075c027f56fae4148bacbf6c78b59df09f84b07b',
-					privateKeyEncrypted: '6f1a067690b4481de3743de3f015da5f172d939b5b1b4842c16977278a9c1fb914adc6079df87c70ab6cef422d6add01',
-					publicKey: '033f4031d22289befe017472bb954b59d9ba043ce67fbc60c50ee3a48c56b89b1f'
+					encryptedPrivateKey: '6f1a067690b4481de3743de3f015da5f172d939b5b1b4842c16977278a9c1fb914adc6079df87c70ab6cef422d6add01',
+					prikey: '2d5ed8706749f6d7c096772a075c027f56fae4148bacbf6c78b59df09f84b07b',
+					pubKey: '033f4031d22289befe017472bb954b59d9ba043ce67fbc60c50ee3a48c56b89b1f'
 				});
 			});
 		});

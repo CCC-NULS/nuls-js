60ddefb47d6d80764e8d090b34fc65b45f1fb0e3
Josh Welham <josh@aluminati.net>
2018-10-25 22:53:03 +0100

    test throwing an error

--- src/packages/core/__tests__/QRCode.spec.ts
+++ src/packages/core/__tests__/QRCode.spec.ts
@@ -1,9 +1,8 @@
-import { QRCode } from '@/index';
-
 describe('creating QR codes', () =>
 {
 	test('an address', async () =>
 	{
+		const { QRCode } = require('@/index');
 		const code = await QRCode.create('Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6');
 
 		expect(code).toMatchSnapshot();
@@ -11,8 +10,32 @@ describe('creating QR codes', () =>
 
 	test('an address with an amount', async () =>
 	{
+		const { QRCode } = require('@/index');
 		const code = await QRCode.create('Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6', { amount: 10 });
 
 		expect(code).toMatchSnapshot();
+	});
+
+	test('throw error', async () =>
+	{
+		const { QRCode } = require('@/index');
+		const PkgQRCode = require('qrcode');
+		let errorMessage = 'No error thrown.';
+
+		PkgQRCode.toDataURL = jest.fn(() =>
+		{
+			throw new Error('Error message');
+		});
+
+		try
+		{
+			await await QRCode.create('Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6', { amount: 10 });
+		}
+		catch(error)
+		{
+			errorMessage = error.message;
+		}
+
+		expect(errorMessage).toBe('Error message');
 	});
 });

59e4045fcc12ae006125d6d54700d1703870300b
Josh Welham <josh@aluminati.net>
2018-11-09 22:34:14 +0000

    parent class for handling any api calls

--- src/packages/api/APIServer.ts
+++ src/packages/api/APIServer.ts
@@ -1,3 +1,20 @@
+import axios, { AxiosInstance } from 'axios';
+
 export const APIServer = 'https://apiserver.nuls.io/';
 
 export const APIServerTestNet = 'http://testnet.apiserver.nuls.io/';
+
+export class APIServerClass
+{
+	public readonly url: string;
+	public readonly api: AxiosInstance;
+
+	constructor(url: string = APIServer)
+	{
+		this.url = url;
+		this.api = axios.create({
+			baseURL: this.url,
+			timeout: 5000
+		});
+	}
+}

bf84c44681ae29a30dd5a5a98fad291eb31b0874
Josh Welham <josh@aluminati.net>
2018-11-10 11:46:37 +0000

    removed duplicated test

--- src/packages/api/__tests__/Transfer.spec.ts
+++ src/packages/api/__tests__/Transfer.spec.ts
@@ -6,19 +6,6 @@ describe('transfer', () =>
 		jest.clearAllMocks();
 	});
 
-	test('the api server is optional and has our default value', async () =>
-	{
-		const { Transfer, APIServer, APIServerTestNet } = require('@/index');
-
-		const transferDefault = new Transfer();
-		const transferLive = new Transfer(APIServer);
-		const transferTest = new Transfer(APIServerTestNet);
-
-		expect(transferDefault.url).toEqual('https://apiserver.nuls.io/');
-		expect(transferLive.url).toEqual('https://apiserver.nuls.io/');
-		expect(transferTest.url).toEqual('http://testnet.apiserver.nuls.io/');
-	});
-
 	test('send a transaction', async () =>
 	{
 		const { Transfer } = require('@/index');

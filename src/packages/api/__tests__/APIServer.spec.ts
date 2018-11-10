f7546d6f9f6081c81b6a8c50cd7a79314365587c
Josh Welham <josh@aluminati.net>
2018-11-10 11:47:13 +0000

    test new api urls and added custom url testing

--- src/packages/api/__tests__/APIServer.spec.ts
+++ src/packages/api/__tests__/APIServer.spec.ts
@@ -1,3 +1,5 @@
+const { APIServerClass, APIServer, APIServerTestNet } = require('@/index');
+
 describe('APIServer', () =>
 {
 	beforeEach(() =>
@@ -6,16 +8,23 @@ describe('APIServer', () =>
 		jest.clearAllMocks();
 	});
 
-	test('the api server is optional and has our default value', async () =>
+	test('the api server is optional and has our default value', () =>
 	{
-		const { APIServerClass, APIServer, APIServerTestNet } = require('@/index');
-
 		const APIDefault = new APIServerClass();
 		const APILive = new APIServerClass(APIServer);
 		const APITest = new APIServerClass(APIServerTestNet);
 
-		expect(APIDefault.url).toEqual('https://apiserver.nuls.io/');
-		expect(APILive.url).toEqual('https://apiserver.nuls.io/');
-		expect(APITest.url).toEqual('http://testnet.apiserver.nuls.io/');
+		expect(APIDefault.url).toEqual('https://apiserver.nuls.io/nuls');
+		expect(APILive.url).toEqual('https://apiserver.nuls.io/nuls');
+		expect(APITest.url).toEqual('http://testnet.apiserver.nuls.io/nuls');
+	});
+
+	test('custom api urls', () =>
+	{
+		const foo = new APIServerClass('foo');
+		const bar = new APIServerClass('bar/');
+
+		expect(foo.url).toEqual('foo');
+		expect(bar.url).toEqual('bar');
 	});
 });

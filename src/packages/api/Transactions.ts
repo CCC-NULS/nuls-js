8438b8912a54c74e5d0d3c120dbe9dd914626571
Josh Welham <josh@aluminati.net>
2018-11-10 11:47:30 +0000

    changed url path

--- src/packages/api/Transactions.ts
+++ src/packages/api/Transactions.ts
@@ -4,7 +4,7 @@ export class Transactions extends APIServerClass
 {
 	public async getBalance(address)
 	{
-		const res = await this.api.get(`nuls/balance/get/${address}`);
+		const res = await this.api.get(`/balance/get/${address}`);
 
 		return res;
 	}

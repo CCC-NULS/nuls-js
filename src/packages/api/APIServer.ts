6a730b2b91c0318278294643a070523abbc9e212
amalcaraz <amalcaraz89@gmail.com>
2018-11-17 14:17:09 +0100

    Added API resources config

--- src/packages/api/APIServer.ts
+++ src/packages/api/APIServer.ts
@@ -5,14 +5,17 @@ export interface IAPIConfig {
 	host: string;
 	port: number;
 	base: string;
+	resources: Record<string, string>;
 }
 
 export class APIServerClass {
-	public url: string;
+	public readonly url: string;
 	public readonly api: AxiosInstance;
+	public readonly resources: Record<string, string>;
 
 	constructor(apiConf: IAPIConfig = config.api.apiserver) {
 		this.url = `${apiConf.host}${apiConf.port ? (':' + apiConf.port) : ''}${apiConf.base}`;
+		this.resources = apiConf.resources;
 		this.api = axios.create({
 			baseURL: this.url,
 			timeout: config.api.timeout

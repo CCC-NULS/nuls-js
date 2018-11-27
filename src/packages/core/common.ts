87ba53dd4ac1a20de59556168c7d01da03a1278e
amalcaraz <amalcaraz89@gmail.com>
2018-11-27 23:06:00 +0100

    Added Transaction types and status enums

--- src/packages/core/common.ts
+++ src/packages/core/common.ts
@@ -10,3 +10,23 @@ export const UNIT_FEE = 1000000;
 export const KB = 1024;
 
 export const PLACE_HOLDER = Buffer.from([255, 255, 255, 255]);
+
+// http://dev.nuls.io/protocol/transaction.html#ID-%E5%AE%9A%E4%B9%89
+export enum TransactionType {
+  Reward = 1,     // consensus reward
+  Transfer = 2,   // transfer transaction
+  Aliased = 3,    // set alias
+  Register = 4,   // register consensus node
+  Deposit = 5,    // join consensus
+  Withdraw = 6,   // cancel consensus
+  YellowCard = 7, // yellow card
+  RedCard = 8,    // red card
+  Unregister = 9, // unregister consensus node
+}
+
+export enum TransactionStatus {
+  UnspentFree = 0,
+  UnspentLocked = 1,
+  Unspent = 2,
+  Spent = 3,
+}

a570227bb2c5ff5a07edd96f17fdb773c3855908
amalcaraz <amalcaraz89@gmail.com>
2018-11-28 01:13:53 +0100

    Refactored utils/crypto module

--- src/packages/core/utils/crypto.ts
+++ src/packages/core/utils/crypto.ts
@@ -1,4 +1,7 @@
+import * as bs58 from 'bs58';
+import * as RIPEMD160 from 'ripemd160';
 import * as secp256k1 from 'secp256k1';
+import * as shajs from 'sha.js';
 import { PRIVATE_KEY_LENGTH } from '../common';
 import { isHex } from './serialize';
 
@@ -35,4 +38,31 @@ export function checkPrivateKey(privateKey: string): boolean {
 
 export function privateKeyToPublicKey(prv: Buffer): Buffer {
   return secp256k1.publicKeyCreate(prv);
+}
+
+export function getXOR(bytes: Buffer): number {
+  return bytes.reduce((xor: number, value: number) => xor ^ value);
+}
+
+export function addressFromHash(hash: Buffer): string {
+  return bs58.encode(Buffer.concat([hash, Buffer.from([getXOR(hash)])]));
+}
+
+export function hashFromAddress(address: string): Buffer {
+  const hash: number[] = bs58.decode(address);
+  return Buffer.from(hash.slice(0, hash.length - 1));
+}
+
+export function hashX2(buffer: Buffer): Buffer {
+  const sha = new shajs.sha256().update(buffer).digest();
+  return new shajs.sha256().update(sha).digest();
+}
+
+export function hashFromPublicKey(publicKey: Buffer, { chainId = 8964, addressType = 1 } = {}): Buffer {
+  const sha = new shajs.sha256().update(publicKey).digest();
+  const pubkeyHash = new RIPEMD160().update(sha).digest();
+  const output = Buffer.allocUnsafe(3);
+  output.writeInt16LE(chainId, 0);
+  output.writeInt8(addressType, 2);
+  return Buffer.concat([output, pubkeyHash]);
 }

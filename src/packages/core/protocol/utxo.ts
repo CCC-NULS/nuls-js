8d0d8c124eeb27c7c2966580b327f3c2d7106c71
amalcaraz <amalcaraz89@gmail.com>
2018-12-20 19:03:31 +0100

    New utxo protocol class to handle utxo operations

--- src/packages/core/protocol/utxo.ts
+++ src/packages/core/protocol/utxo.ts
@@ -1,6 +1,20 @@
+import { IAPIConfig } from '..';
+import { UtxoApi } from '../api/utxo';
+
 export interface UTXO {
   hash: string;
   idx: number;
   value: number;
   lockTime: number;
+}
+
+export class Utxo {
+
+  static async getUtxos(address: string, config?: IAPIConfig) {
+
+    const api = new UtxoApi(config);
+    return api.getUtxos(address);
+
+  }
+
 }

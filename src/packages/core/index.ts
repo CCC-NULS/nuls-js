b80aaad7ede2c7c8102dc2ccebf10cb47a6205a0
amalcaraz <amalcaraz89@gmail.com>
2019-03-05 02:11:56 +0100

    More fixes

--- src/packages/core/index.ts
+++ src/packages/core/index.ts
@@ -1,6 +1,6 @@
 export * from './api';
 export * from './account';
-export * from './qqrCode';
+export * from './qrCode';
 export * from './protocol';
 export * from './utils';
 export * from './common';

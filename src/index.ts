8e7309bbf67024d701e20c4fe9695c85587dbf34
Josh Welham <josh@aluminati.net>
2019-03-09 17:58:28 +0000

    allow es6 import usage

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 // https://github.com/Microsoft/TypeScript/pull/29262
 /// <reference path="./public-typings.d.ts" />
+const packages = require('./packages');
+
 export * from './packages';
+
+export default {
+	...packages
+};

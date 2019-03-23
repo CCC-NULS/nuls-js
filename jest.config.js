bdd5d4ac7a2724e5079f42fb92ec0f14c3dfeb7c
Josh Welham <josh@aluminati.net>
2019-03-23 20:21:18 +0000

    tell jest to mock images

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,11 @@ module.exports = {
 	moduleNameMapper: {
 		'^@/(.*)$': '<rootDir>/src/$1',
 		'^__testConfigs/(.*)$': '<rootDir>/testConfigs/$1'
-	},
+    },
+    moduleNameMapper: {
+        '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/__mocks__/fileMock.js',
+        '\\.(css|less)$': '<rootDir>/src/__mocks__/fileMock.js'
+    },
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 	collectCoverageFrom: [
 		'src/**/*.ts',

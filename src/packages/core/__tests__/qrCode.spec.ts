f4829ca6259d6a0b903b36b7308d527a467d3ea3
Josh Welham <josh@aluminati.net>
2019-03-23 20:28:22 +0000

    removed test

--- src/packages/core/__tests__/qrCode.spec.ts
+++ src/packages/core/__tests__/qrCode.spec.ts
@@ -5,90 +5,117 @@ const jimp = require('jimp');
 
 jest.mock('jimp');
 
-xdescribe('creating QR codes', () => {
-	beforeEach(() => {
-		jest.resetModules();
-		jest.clearAllMocks();
-	});
+describe('creating QR codes', () => {
 
-	test('an address', async () => {
-		const code = await QRCode.create('Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6');
+  beforeEach(() => {
 
-		expect(jimp.read.mock.calls).toMatchSnapshot();
-		expect(jimp.read().composite.mock.calls).toMatchSnapshot();
-		expect(PkgQRCode.toDataURL.mock.calls).toMatchSnapshot();
-		expect(code).toEqual('baz');
-	});
+    jest.resetModules();
+    jest.clearAllMocks();
 
-	test('an address with an amount', async () => {
-		const code = await QRCode.create('Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6', { amount: 10 });
+  });
 
-		expect(jimp.read.mock.calls).toMatchSnapshot();
-		expect(jimp.read().composite.mock.calls).toMatchSnapshot();
-		expect(PkgQRCode.toDataURL.mock.calls).toMatchSnapshot();
-		expect(code).toEqual('baz');
-	});
+  test('an address', async () => {
 
-	test('private ket import', async () => {
-		const code = await QRCode.create('5af6f2f9aa014d467c22a7523ed352a0eaed4816b4caa3cc52c15d68d627089db055f95d3de88e01fab28d2fa96fb10e');
+    const code = await QRCode.create('Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6');
 
-		expect(jimp.read.mock.calls).toMatchSnapshot();
-		expect(jimp.read().composite.mock.calls).toMatchSnapshot();
-		expect(PkgQRCode.toDataURL.mock.calls).toMatchSnapshot();
-		expect(code).toEqual('baz');
-	});
+    expect(jimp.read.mock.calls).toMatchSnapshot();
+    expect(jimp.read().composite.mock.calls).toMatchSnapshot();
+    expect(PkgQRCode.toDataURL.mock.calls).toMatchSnapshot();
+    expect(code).toEqual('baz');
 
-	test('error correction level option setting `L`', async () => {
-		const code = await QRCode.create('Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6', undefined, undefined, { errorCorrectionLevel: 'L' });
+  });
 
-		expect(jimp.read.mock.calls).toMatchSnapshot();
-		expect(jimp.read().composite.mock.calls).toMatchSnapshot();
-		expect(PkgQRCode.toDataURL.mock.calls).toMatchSnapshot();
-		expect(code).toEqual('baz');
-	});
+  test('an address with an amount', async () => {
 
-	test('logo colours', async () => {
-		const green = await QRCode.create('Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6');
-		const white = await QRCode.create('Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6', undefined, { logo: LogoColour.green });
-		const black = await QRCode.create('Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6', undefined, { logo: LogoColour.black });
-		const blank = await QRCode.create('Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6', undefined, { logo: false });
+    const code = await QRCode.create('Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6', { amount: 10 });
 
-		expect(jimp.read.mock.calls).toMatchSnapshot();
-		expect(jimp.read().composite.mock.calls).toMatchSnapshot();
-		expect(PkgQRCode.toDataURL.mock.calls).toMatchSnapshot();
-		expect(green).toEqual('baz');
-		expect(white).toEqual('baz');
-		expect(black).toEqual('baz');
-		expect(blank).toEqual('foo');
-	});
+    expect(jimp.read.mock.calls).toMatchSnapshot();
+    expect(jimp.read().composite.mock.calls).toMatchSnapshot();
+    expect(PkgQRCode.toDataURL.mock.calls).toMatchSnapshot();
+    expect(code).toEqual('baz');
 
-	test('unknown logo colour', async () => {
-		let errorMessage = 'No error thrown.';
+  });
 
-		try {
-			await QRCode.create('Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6', undefined, { logo: 'foo' as any });
-		}
-		catch (error) {
-			errorMessage = error.message;
-		}
+  test('private ket import', async () => {
 
-		expect(errorMessage).toBe('Unknown NULS logo colour [foo]');
-	});
+    const code = await QRCode.create('5af6f2f9aa014d467c22a7523ed352a0eaed4816b4caa3cc52c15d68d627089db055f95d3de88e01fab28d2fa96fb10e');
 
-	test('throw error', async () => {
-		let errorMessage = 'No error thrown.';
+    expect(jimp.read.mock.calls).toMatchSnapshot();
+    expect(jimp.read().composite.mock.calls).toMatchSnapshot();
+    expect(PkgQRCode.toDataURL.mock.calls).toMatchSnapshot();
+    expect(code).toEqual('baz');
 
-		PkgQRCode.toDataURL = jest.fn(() => {
-			throw new Error('Error message');
-		});
+  });
 
-		try {
-			await QRCode.create('Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6', { amount: 10 });
-		}
-		catch (error) {
-			errorMessage = error.message;
-		}
+  test('error correction level option setting `L`', async () => {
 
-		expect(errorMessage).toBe('Error message');
-	});
+    const code = await QRCode.create('Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6', undefined, undefined, { errorCorrectionLevel: 'L' });
+
+    expect(jimp.read.mock.calls).toMatchSnapshot();
+    expect(jimp.read().composite.mock.calls).toMatchSnapshot();
+    expect(PkgQRCode.toDataURL.mock.calls).toMatchSnapshot();
+    expect(code).toEqual('baz');
+
+  });
+
+  test('logo colours', async () => {
+
+    const green = await QRCode.create('Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6');
+    const white = await QRCode.create('Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6', undefined, { logo: LogoColour.green });
+    const black = await QRCode.create('Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6', undefined, { logo: LogoColour.black });
+    const blank = await QRCode.create('Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6', undefined, { logo: false });
+
+    expect(jimp.read.mock.calls).toMatchSnapshot();
+    expect(jimp.read().composite.mock.calls).toMatchSnapshot();
+    expect(PkgQRCode.toDataURL.mock.calls).toMatchSnapshot();
+    expect(green).toEqual('baz');
+    expect(white).toEqual('baz');
+    expect(black).toEqual('baz');
+    expect(blank).toEqual('foo');
+
+  });
+
+  test('unknown logo colour', async () => {
+
+    let errorMessage = 'No error thrown.';
+
+    try {
+
+      await QRCode.create('Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6', undefined, { logo: 'foo' as any });
+
+    } catch (error) {
+
+      errorMessage = error.message;
+
+    }
+
+    expect(errorMessage).toBe('Unknown NULS logo colour [foo]');
+
+  });
+
+//   test('throw error', async () => {
+
+//     let errorMessage = 'No error thrown.';
+
+//     PkgQRCode.toDataURL = jest.fn(() => {
+
+//       throw new Error('Error message');
+
+// 	});
+
+
+//     try {
+
+// 		await QRCode.create('Nse1TYHc6Rxs84iimrnygSF2kqrUAQM6', { amount: 10 });
+
+//     } catch (error) {
+
+//       errorMessage = error.message;
+
+// 	}
+
+//     expect(errorMessage).toBe('Error message');
+
+//   });
+
 });

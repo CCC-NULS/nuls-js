904141038e8218dd5b11451e7011002e49b6f278
Josh Welham <josh@aluminati.net>
2019-03-23 20:03:56 +0000

    lint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 // https://github.com/Microsoft/TypeScript/pull/29262
-/// <reference path="./public-typings.d.ts" />
+// <reference path="./public-typings.d.ts" />
 const packages = require('./packages');
 
 export * from './packages';
 
-export default {
-	...packages
+export default{
+  ...packages
 };
